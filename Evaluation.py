97cf8a3b492e61427a4e39690be87884db43d093
Piotr Biskup <pbiskup@egnyte.com>
2020-01-23 13:36:52 +0100

    saving curves to png

--- Evaluation.py
+++ Evaluation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from os import listdir
 from os.path import isfile, join
@@ -33,7 +34,7 @@ def get_dfs_from_files(dir_path, files_names, only_tail, tail_len):
     return data_frames
 
 
-def get_dfs_from_files_with_headers(dir_path, files_names, only_tail, tail_len):
+def get_dfs_from_files_with_headers(dir_path, files_names, only_tail=False, tail_len=None):
     data_frames = []
 
     for file in files_names:
@@ -80,12 +81,8 @@ def process_pred_files(ad_num):
     batch_pred_path = '/Users/pbiskup/Documents/Inz/pred/batch/b' + str(ad_num) + '/'
     sm_file_name = 'stream_metrics_' + str(ad_num) + '.csv'
     bm_file_name = 'batch_metrics_' + str(ad_num) + '.csv'
-    stream_rocc_file_name = 'stream_roc_curve_' + str(ad_num) + '.csv'
-    batch_rocc_file_name = 'batch_roc_curve_' + str(ad_num) + '.csv'
-    random_rocc_file_name = 'random_roc_curve_' + str(ad_num) + '.csv'
-    stream_prc_file_name = 'stream_pr_curve_' + str(ad_num) + '.csv'
-    batch_prc_file_name = 'batch_pr_curve_' + str(ad_num) + '.csv'
-    random_prc_file_name = 'random_pr_curve_' + str(ad_num) + '.csv'
+    roc_curve_name = 'roc_curve_' + str(ad_num) + '.png'
+    pr_curve_name = 'pr_curve_' + str(ad_num) + '.png'
 
     print('Retrieving files names from directories...')
 
@@ -118,35 +115,72 @@ def process_pred_files(ad_num):
                                                                       random_pred_df['pred'])
     random_precision, random_recall, random_prc_thresholds = metrics.precision_recall_curve(
         random_pred_df['true_label'], random_pred_df['pred'])
-
-    stream_roc_curve_df = pd.DataFrame(data={'stream_fpr': stream_metrics[0].roc_curve.fpr,
-                                             'stream_tpr': stream_metrics[0].roc_curve.tpr})
-
-    batch_roc_curve_df = pd.DataFrame(data={'batch_fpr': batch_metrics[0].roc_curve.fpr,
-                                            'batch_tpr': batch_metrics[0].roc_curve.tpr})
-
-    random_roc_curve_df = pd.DataFrame(data={'random_fpr': random_fpr,
-                                             'random_tpr': random_tpr})
-
-    stream_pr_curve_df = pd.DataFrame(data={'stream_precision': stream_metrics[0].pr_curve.precision,
-                                            'stream_recall': stream_metrics[0].pr_curve.recall})
-
-    batch_pr_curve_df = pd.DataFrame(data={'batch_precision': batch_metrics[0].pr_curve.precision,
-                                           'batch_recall': batch_metrics[0].pr_curve.recall})
-
-    random_pr_curve_df = pd.DataFrame(data={'random_precision': random_precision,
-                                            'random_recall': random_recall})
+    random_avg_prec = metrics.average_precision_score(random_pred_df['true_label'], random_pred_df['pred'])
 
     stream_metrics_df.to_csv(join(output_path, sm_file_name), index=False)
     batch_metrics_df.to_csv(join(output_path, bm_file_name), index=False)
 
-    stream_roc_curve_df.to_csv(join(output_roc_curve_path, stream_rocc_file_name), index=False)
-    batch_roc_curve_df.to_csv(join(output_roc_curve_path, batch_rocc_file_name), index=False)
-    random_roc_curve_df.to_csv(join(output_roc_curve_path, random_rocc_file_name), index=False)
+    #  roc curve
 
-    stream_pr_curve_df.to_csv(join(output_pr_curve_path, stream_prc_file_name), index=False)
-    batch_pr_curve_df.to_csv(join(output_pr_curve_path, batch_prc_file_name), index=False)
-    random_pr_curve_df.to_csv(join(output_pr_curve_path, random_prc_file_name), index=False)
+    plt.plot(stream_metrics[0].roc_curve.fpr, stream_metrics[0].roc_curve.tpr,
+             label='stream ROC curve (area = %0.3f)' % stream_metrics[0].roc_curve.roc_auc)
+
+    plt.plot(batch_metrics[0].roc_curve.fpr, batch_metrics[0].roc_curve.tpr,
+             label='batch ROC curve (area = %0.3f)' % batch_metrics[0].roc_curve.roc_auc)
+
+    plt.plot(random_fpr, random_tpr,
+             label='random ROC curve (area = %0.3f)' % metrics.auc(random_fpr, random_tpr))
+
+    plt.plot([0, 1], [0, 1], 'k--')  # random predictions curve
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.xlabel('False Positive Rate or (1 - Specifity)')
+    plt.ylabel('True Positive Rate or (Sensitivity)')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc="lower right")
+
+    plt.savefig(join(output_path, roc_curve_name))
+    plt.clf()
+
+    #  precision recall curve
+
+    y_limit = 1.0
+    if ad_num == 1:
+        y_limit = 0.6
+    elif ad_num == 2:
+        y_limit = 0.007
+    elif ad_num == 3:
+        y_limit = 0.004
+    elif ad_num == 4:
+        y_limit = 0.0035
+    elif ad_num == 5:
+        y_limit = 0.06
+    elif ad_num == 6:
+        y_limit = 0.6
+    elif ad_num == 7:
+        y_limit = 0.1
+    elif ad_num == 8:
+        y_limit = 0.6
+    elif ad_num == 9:
+        y_limit = 0.007
+
+    plt.plot(stream_metrics[0].pr_curve.recall, stream_metrics[0].pr_curve.precision, marker='.',
+             label='stream PR curve (avg prec = %0.3f)' % stream_metrics[0].avg_prec, zorder=5, linewidth=1,
+             markersize=1)
+
+    plt.plot(batch_metrics[0].pr_curve.recall, batch_metrics[0].pr_curve.precision, marker='.',
+             label='batch PR curve (avg prec = %0.3f)' % batch_metrics[0].avg_prec, zorder=5, linewidth=1, markersize=1)
+
+    plt.plot(random_recall, random_precision, marker='.',
+             label='random PR curve (avg prec = %0.3f)' % random_avg_prec, zorder=5, linewidth=1, markersize=1)
+
+    plt.ylim([0.0, y_limit])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.legend(loc="upper right")
+    plt.title('Precision Recall Curve')
+    plt.savefig(join(output_path, pr_curve_name))
+    plt.clf()
 
 
 def filter_files_with_name(files_names, contained_word):
@@ -166,8 +200,8 @@ def combine_metrics_files():
     stream_metrics_file_names.sort()
     batch_metrics_file_names.sort()
 
-    stream_metrics_dfs = get_dfs_from_files_with_headers(output_path, stream_metrics_file_names, False, None)
-    batch_metrics_dfs = get_dfs_from_files_with_headers(output_path, batch_metrics_file_names, False, None)
+    stream_metrics_dfs = get_dfs_from_files_with_headers(output_path, stream_metrics_file_names)
+    batch_metrics_dfs = get_dfs_from_files_with_headers(output_path, batch_metrics_file_names)
 
     stream_df = concat_data_frames(stream_metrics_dfs)
     batch_df = concat_data_frames(batch_metrics_dfs)
@@ -182,7 +216,7 @@ def combine_metrics_files():
     result_df.to_csv(join(output_path, 'metrics.csv'), index=False)
 
 
-# for x in range(1, 10):
-#     process_pred_files(x)
+for x in range(1, 10):
+    process_pred_files(x)
 
-# combine_metrics_files()
+combine_metrics_files()
